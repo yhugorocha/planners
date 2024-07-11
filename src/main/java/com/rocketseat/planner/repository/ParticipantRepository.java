49de0b04098b0191d41cb7621c4534a58cd89d1b
Hugo Silva <yhugorocha@gmail.com>
2024-07-11 18:37:14 -0300

    FEAT: Add implementation of the trip id search method

--- src/main/java/com/rocketseat/planner/repository/ParticipantRepository.java
+++ src/main/java/com/rocketseat/planner/repository/ParticipantRepository.java
@@ -4,8 +4,10 @@ import com.rocketseat.planner.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+    List<Participant> findByTripId(UUID tripId);
 }
